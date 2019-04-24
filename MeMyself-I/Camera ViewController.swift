04905f04318ba8d3cf6e49bca2cc990ad1ebbdb5
Hao <haonan@Haos-MacBook-Pro-2.local>
2019-04-24 18:28:12 -0400

    some loyoet

---
+++ MeMyself-I/Camera ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Camera ViewController.swift
+//  MeMyself-I
+//
+//  Created by Hao on 4/24/19.
+//  Copyright © 2019 Hao. All rights reserved.
+//
+
+import UIKit
+
+class Camera_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MeMyself-I/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  MeMyself-I
+//
+//  Created by Hao on 4/24/19.
+//  Copyright © 2019 Hao. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
